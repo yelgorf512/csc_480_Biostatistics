52f81ae184dc5c420d3c2c329833328d4f7a150b
nickfro512 <nickfro@gmail.com>
2018-01-28 10:12:21 -0500

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,23 @@
+### CSC 480 Assignment 1
+### Author: Nick Frogley
+
+import os
+
+# load in file contents
+infile = open("sequences.txt")
+seqs = infile.read()
+infile.close()
+
+lines = seqs.split("\n")
+
+i = 1;
+
+# look for sequence in each line of file and print info about it
+for the_line in lines:
+    colon_position = the_line.find(":")
+    position = the_line.find("GGGAAC")
+    if position != -1:
+        print ("Sample: " + str(i) + "\tPosition:"+ str(position - colon_position - 1))
+        print ("Sequence after: " + the_line[position::])
+    i += 1
+    
