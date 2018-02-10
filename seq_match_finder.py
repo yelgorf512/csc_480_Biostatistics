4363c9433b086f904151498ce8cb591256c9a151
nickfro512 <nickfro@gmail.com>
2018-02-10 17:46:49 -0500

    Added pairwise alignment
    This script produces a CSV of pairwise alignment scores for every sequence now.

--- seq_match_finder.py
+++ seq_match_finder.py
@@ -6,6 +6,10 @@
 # FASTA file of sequences and writes it to a CSV file
 
 from Bio import SeqIO # to parse sequence data
+from Bio.Seq import Seq
+
+from Bio import pairwise2
+from Bio.pairwise2 import format_alignment
 
 import io # to convert string to 'handle'
 
@@ -46,6 +50,37 @@ class MatchRecord :
         csv_str += self.upstream_str
         return csv_str
 
+# this class stores info about an alignment
+class AlignmentRecord :
+
+    id_str = ""         # the id from the FASTA record
+    score = 0           # alignment score
+    strand = -1         # strand; 0 = original, 1 = reverse complement
+    
+    # set values
+    def __init__(self, new_id, new_score, new_strand) :    
+        self.id_str = new_id
+        self.score = new_score
+        self.strand = new_strand
+
+    # print values
+    def printValues(self) :
+        print("ID: " + self.id_str)
+        print("SCORE: " + str(self.score))
+        if (self.strand == 0) :
+            print("FOUND IN ORIGINAL")
+        else :
+            print("FOUND IN REVERSE COMPLEMENT")
+
+    # return values as CSV row
+    def getCSV(self) :
+        csv_str =   self.id_str + ","
+        csv_str +=  str(self.score) + ","
+        if (self.strand == 0) :
+            csv_str += "ORIGINAL" + ","
+        else :
+            csv_str += "REVERSE COMPLEMENT" + ","
+        return csv_str
 
 # get matches between target sequences and sequences from a file that are
 # in fasta format
@@ -84,15 +119,59 @@ def getHits(target_seq, file) :
     print("# matches found:", len(records))
     return records                
 
+# Get pairwise alignments with a target sequence for each sequence in a FASTA file 
+def getAlignments(target_seq, rc_target_seq, file) :
 
-def outputRecords(records, file = None) :
+    # parse sequences in 'fasta' format; this returns an iterator, which stores
+    # a sequence of elements
+    sequences = SeqIO.parse(file, 'fasta')
+
+    records = list()    # stores our AlignmentRecord objects (sequence matches)
+
+    target_seq_len = len(target_seq)
+
+    # loop through sequence records in the file
+    counter = 1
+    for s in sequences:
+    
+        if len(s) < target_seq_len:    
+            continue
+
+        # get alignment for the original version of this sequence
+        if counter % 100 == 0 :
+            print ("GETTING ALIGNMENT FOR SEQ " + str(counter) + "...")
+
+        alignments = pairwise2.align.globalms(target_seq, s, 2,-1, -2, -1, 
+                                      penalize_end_gaps = (False, True))
+
+        the_record = AlignmentRecord(s.id, alignments[0][2], 0)
+        #the_record.printValues()
+        records.append(the_record)
+
+        # get alignment for the reverse complement of this sequence
+        alignments = pairwise2.align.globalms(rc_target_seq, s, 2,-1, -2, -1, 
+                                      penalize_end_gaps = (False, True))
+
+        the_record = AlignmentRecord(s.id, alignments[0][2], 1)
+        #the_record.printValues()
+        records.append(the_record)
+        
+        counter += 1
+        
+    return records                
+
+
+def outputRecords(records, file = None, alignments = False) :
     if file == None :
         for r in records :
             r.printValues()        
     else :
         # output matches to CSV file
         outfile = open(file,"w")
-        outfile.write("ID,POSITION,STRAND,UPSTREAM\n")
+        if (alignments == False) :
+            outfile.write("ID,POSITION,STRAND,UPSTREAM\n")
+        else :
+            outfile.write("ID,SCORE,STRAND\n")
         for r in records :
             outfile.write(r.getCSV() + "\n")
         outfile.close();
@@ -102,19 +181,22 @@ el2 = "actgacatccggacagcgttgcgacagtggcgcttttagcgcagcccgggggtttttacaggatacc".uppe
 el3 = "gtggcgcttttagcgcagcccgggggtttttacaggatacca".upper()
 el321 = "AATTGAGGTGGATCGGTGGATCGGTGGATCAGTTCATTTCGGAACTGAAATGAGCCGTGTCCGAGGTGAGTCCGGAAATGGGCTCAAAACTGCGGTGAAACCACTGACATCCGGACAGCGTTGCGACAGTGGCGCTTTTAGCGCAGCCCGGGGGTTTTTACAGGATACC"
 
-res1 = getHits(el1, "seqs.fa")
-outputRecords(res1, "el1_results.csv")
+rc_el321 = Seq(el321).reverse_complement()
 
-res2 = getHits(el2, "seqs.fa")
-outputRecords(res2, "el2_results.csv")
+#res1 = getHits(el1, "seqs.fa")
+#outputRecords(res1, "el1_results.csv")
 
-res3 = getHits(el3, "seqs.fa")
-outputRecords(res3, "el3_results.csv")
+#res2 = getHits(el2, "seqs.fa")
+#outputRecords(res2, "el2_results.csv")
 
-res321 = getHits(el321, "seqs.fa")
-outputRecords(res321, "el321_results.csv")
+#res3 = getHits(el3, "seqs.fa")
+#outputRecords(res3, "el3_results.csv")
 
+#res321 = getHits(el321, "seqs.fa")
+#outputRecords(res321, "el321_results.csv")
 
+alignments = getAlignments(el321, rc_el321, "seqs.fa")
+outputRecords(alignments, "el312_alignment_results.csv", True)
 
 
     
