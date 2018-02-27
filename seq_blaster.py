4ea9ccf5dd2583ab8aff3fdcc0d1dbc5caea8e5b
nickfro512 <nickfro@gmail.com>
2018-02-27 12:09:10 -0500

    Added query start position to BLAST results

--- seq_blaster.py
+++ seq_blaster.py
@@ -4,20 +4,9 @@ from Bio.Blast import NCBIXML
 
 import sys
 
-# Assignment #2:
 
-# write a script that reads in a results file from the
-# match script, and for each sequence, finds the top BLAST hit.
-
-# Create a new file that copies the original input file and adds the
-# following columns: the definition (.hit_def).
-
-# Then for the first high-scoring pair (from topHit.hsps), get the alignment
-# length and the percent identity (the number of identities / the alignment
-# length).
-
-# This script should take two arguments – the input file name and
-# output file name. An example BLAST script is below:
+# reads in a results file from the seq_match_finder script, and for each sequence, 
+# finds the top BLAST hit, and adds information about that hit to the original results file.
 
 def get_csv_lines(data_file):
     infile = open(data_file)
@@ -44,7 +33,7 @@ if len(sys.argv) == 3 :
 else:
     print("Missing args (input file, output file), running with hardcoded values")
     input_file = "direct_matches.csv"
-    output_file = "blast_results.csv"
+    output_file = "blast_results_4.csv"
 
 
 seq_lines = get_csv_lines(input_file)
@@ -54,13 +43,13 @@ seq_list = list()
 
 skip = 1
 counter = 0
-limit = -1  
+limit = -2  
 
 for the_line in seq_lines:
     comma_split_line = the_line.split(",")
-    if counter >= skip and len(comma_split_line) == 4 :
+    if counter >= skip and len(comma_split_line) == 5 :
         print('Adding sequence ' + str(counter))
-        seq_list.append(comma_split_line[3])
+        seq_list.append(comma_split_line[4])
     else :
         seq_list.append("")
     counter += 1
@@ -68,7 +57,7 @@ for the_line in seq_lines:
         break
 
 new_csv_lines = list()
-new_csv_lines.append("ID, POSITION, STRAND, UPSTREAM, TOP DEFINITION, ALIGN LENGTH, PERCENT IDENTITY")
+new_csv_lines.append("ID, POSITION, STRAND, UPST LENGTH, UPSTREAM, TOP DEFINITION, ALIGN LENGTH, PERCENT IDENTITY, Q START")
 
 counter = 0
 
@@ -88,6 +77,7 @@ for seq in seq_list :
         result_handle = open("blast_result_ " + str(counter) + ".xml")
         #result_handle = open("my_blast.xml")
         blast_record = NCBIXML.read(result_handle)
+
         topHit = blast_record.alignments[0]
 
         # look at first high scoring pair (hsp)
@@ -96,7 +86,8 @@ for seq in seq_list :
         new_csv_line = seq_lines[counter] + ","
         new_csv_line += topHit.hit_def.replace(",", "|") + ","
         new_csv_line += str(hsp.align_length) + ","
-        new_csv_line += str(hsp.identities/hsp.align_length * 100)
+        new_csv_line += str(hsp.identities/hsp.align_length * 100) + ","
+        new_csv_line += str(hsp.query_start)
         new_csv_lines.append(new_csv_line)
 
     elif (counter >= skip) :
@@ -107,38 +98,3 @@ for seq in seq_list :
 
 
 output_csv_lines(new_csv_lines, output_file)
-
-exit(1)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# read in sequence and BLAST it
-record = SeqIO.read("output.txt", format="fasta")
-result_handle = NCBIWWW.qblast("blastn", "nt", record.format("fasta"))
-
-# save results to file
-file = open("my_blast.xml", "w")
-file.write(result_handle.read())
-file.close()
-
-
-# read in results and parse them,
-# http://biopython.org/DIST/docs/tutorial/Tutorial.html#sec:parsing-blast
-result_handle = open("my_blast.xml")
-blast_record = NCBIXML.read(result_handle)
-topHit = blast_record.alignments[0]
-
-# look at first high scoring pair (hsp)
-hsp = topHit.hsps[0]
